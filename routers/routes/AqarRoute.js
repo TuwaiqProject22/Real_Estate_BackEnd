3077945baed0ee09eb73b0425965b30776c0204a
RawanZaben <rawan.zaben.rae.js@tuwaiq.edu.sa>
2021-12-15 21:45:26 +0300

    authentication

--- routers/routes/AqarRoute.js
+++ routers/routes/AqarRoute.js
@@ -2,9 +2,10 @@ const express = require("express");
 const AqarRoute = express.Router();
 
 const { geAqars, postAqars, deleteAqar } = require("../controllers/Aqar");
+const {authentication} = require("../middlewares/authentication")
 
-AqarRoute.get("/Aqars", geAqars);
-AqarRoute.post("/Aqars", postAqars);
-AqarRoute.delete("/Aqar:id", deleteAqar);
+AqarRoute.get("/Aqars",authentication, geAqars);
+AqarRoute.post("/Aqars",authentication, postAqars);
+AqarRoute.delete("/Aqar:id",authentication, deleteAqar);
 
 module.exports = AqarRoute;
