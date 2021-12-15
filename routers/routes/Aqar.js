bd547526c16b0242618eca012b2566c7f61e942d
RawanZaben <rawan.zaben.rae.js@tuwaiq.edu.sa>
2021-12-15 21:42:59 +0300

    aqar_post

--- routers/routes/Aqar.js
+++ routers/routes/Aqar.js
@@ -10,5 +10,17 @@ const geAqars = async (req,res)=>{
     }
 }
 
+//أضافه عقار
+const postAqars= async (req, res)=>{
+    const {TitleAqar, TypeAqar, imgAqar, LocationAqar, spaceAqar,City, NumberAqar} = req.body;
+    const newAqar = new AqarModel({TitleAqar, TypeAqar, imgAqar, LocationAqar, spaceAqar,City, NumberAqar})
+    try {
+        const savedAqar = await newAqar.save()
+         const  Aqar = await AqarModel.find({});
+        res.status(200).json( Aqar)
+    }catch (error){
+        res.send(error)
+    }
+}
 
 module.exports = { geAqars, postAqars, deleteAqar };
